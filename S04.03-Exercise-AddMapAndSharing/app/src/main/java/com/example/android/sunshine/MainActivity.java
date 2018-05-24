6fe38e95163ba491ab7e7b8e11ee07074634138e
olamide <laleyeolamide@yahoo.com>
2018-05-24 14:05:50 +0100

    Sunhine share

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -223,6 +223,23 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
 
         // TODO (2) Launch the map when the map menu item is clicked
 
+        if(id == R.id.open_map) {
+
+                String addressString = "1600 Ampitheatre Parkway, CA";
+                Uri geoLocation = Uri.parse("geo:0,0?q=" + addressString);
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoLocation);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Log.d(TAG, "Couldn't call " + geoLocation.toString()
+                            + ", no receiving apps installed!");
+                }
+            }
+
+
         return super.onOptionsItemSelected(item);
     }
 }
